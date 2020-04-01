1da5cccb07b7f30dcb7d9c671ae2783cced8427c
Victor Plushko <vicplushko33@gmail.com>
2020-04-01 07:22:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,5 @@ end
 def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.find { |cheese| cheese.include?(cheese_types[0])}
-else
-  returns nil
 end
 end
